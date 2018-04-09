10a5f8ab00d4623ff2c690cca0fa6f19f7f6472a
Mark Page <mark.page@whitelamp.com>
2018-04-09 15:19:32 +0100

    swef          swef-manager                    git push origin master

--- swef-git.sh
+++ swef-git.sh
@@ -9,7 +9,6 @@ function git_error_msg {
 }
 
 
-
 function git_exit {
     # Return user to original working directory and exit with status code
     if [ "$1" != "0" ]
@@ -22,82 +21,129 @@ function git_exit {
 returnDir="$(pwd)"
 
 
+function git_push {
+    cd ..
+    instanceDir="$(ls -1 -d swef-instance-* | head -n 1)"
+    echo "Using $(pwd)/$instanceDir/.swef/swef-git-push.cfg..."
+    if [ ! -f "./$instanceDir/.swef/swef-git-push.cfg" ]
+    then
+        git_error_msg "Configuration not found"
+        git_exit 101
+    fi
+    while read -r line
+    do
+        if [ ! "$line" ]
+        then
+            continue
+        fi
+        if [ "${line:0:1}" = "#" ]
+        then
+            continue
+        fi
+        dir=$(echo $line | awk '{print $2}')
+        cmd="$(echo $line | cut --delimiter=" " --fields=3-)"
+        echo -n "$dir/ [ $cmd ] See status, do that or abort? [s/y/n/a] "
+        read -n 1 ok < /dev/tty
+        echo ""
+        if [ "$ok" = "s" ]
+        then
+            echo "--------"
+            cd $dir
+            git status
+            cd ..
+            echo "--------"
+            echo -n "$dir/ [ $cmd ] Do that or abort? [y/n/a] "
+            read -n 1 ok < /dev/tty
+            echo ""
+        fi
+        if [ "$ok" = "a" ]
+        then
+            return
+        fi
+        if [ "$ok" != "y" ]
+        then
+            continue
+        fi
+        cd $dir
+        if [ "$(echo $cmd | grep "commit$")" ]
+        then
+            echo -e "Swef multi-project update $(date +%Y/%m/%d-%H:%M:%S)" "Commit message: "
+            read msg
+            $cmd -m "$msg"
+        else
+            $cmd
+        fi
+        cd ..
+    done < ./$instanceDir/.swef/swef-git-push.cfg
+}
 
-if [ "$1" != "status" ]
+
+function git_status {
+    # Loop through project types
+    cd ..
+    for projectType in swefland vendorland userland
+    do
+        # Loop through projects
+        for dir in $(ls -1 -d *)
+        do
+            # Ignore paths that are not directories
+            if [ ! -d "$dir" ]
+            then
+                 continue
+            fi
+            # Ignore project if current loop is swefland and project is not
+            if [ "$projectType" = "swefland" ] && [ ! -f  "$dir/.swef-type-swef" ]
+            then
+                 continue
+            fi
+            # Ignore project if current loop is vendorland and project is not
+            if [ "$projectType" = "vendorland" ] && [ ! -f  "$dir/.swef-type-vendor" ]
+            then
+                 continue
+            fi
+            # Ignore project if current loop is userland and project is not
+            if [ "$projectType" = "userland" ]
+            then
+                if [ -f "$dir/.swef-type-swef" ] || [ -f  "$dir/.swef-type-vendor" ]
+                then
+                    continue
+                fi
+            fi
+            echo "--------"
+            # Ignore project if no .git directory
+            if [ ! -d "$dir/.git" ]
+            then
+                echo "Ignoring $dir (has no .git directory)"
+                continue
+            fi
+            # Show git status
+            cd "$dir"
+            check="$(git status | grep "nothing to commit, working directory clean")"
+            if [ "$check" ]
+            then
+                echo "$dir - nothing to commit, working directory clean"
+            else
+                echo "$dir - git status:"
+                git status
+                sleep 1
+            fi
+            cd ..
+        done
+    done
+}
+
+
+if [ "$1" = "status" ]
 then
-    git_error_msg "./swef git status"
-    git_exit 101
+    git_status
+    git_exit 0
 fi
 
 
-# Identify directories
-cd "$(dirname "$0")/.."
-umbrellaDir="$(pwd)"
-
-
-
-# Loop through project types
-for projectType in swefland vendorland userland
-do
-
-    # Loop through projects
-    cd "$umbrellaDir"
-    for dir in $(ls -1 -d *)
-    do
-
-        # Ignore paths that are not directories
-        if [ ! -d "$umbrellaDir/$dir" ]
-        then
-             continue
-        fi
-
-        # Ignore project if current loop is swefland and project is not
-        if [ "$projectType" = "swefland" ] && [ ! -f  "$umbrellaDir/$dir/.swef-type-swef" ]
-        then
-             continue
-        fi
-
-        # Ignore project if current loop is vendorland and project is not
-        if [ "$projectType" = "vendorland" ] && [ ! -f  "$umbrellaDir/$dir/.swef-type-vendor" ]
-        then
-             continue
-        fi
-
-        # Ignore project if current loop is userland and project is not
-        if [ "$projectType" = "userland" ]
-        then
-            if [ -f "$umbrellaDir/$dir/.swef-type-swef" ] || [ -f  "$umbrellaDir/$dir/.swef-type-vendor" ]
-            then
-                continue
-            fi
-        fi
-
-        echo "--------"
-
-        # Ignore project if no .git directory
-        if [ ! -d "$umbrellaDir/$dir/.git" ]
-        then
-            echo "Ignoring $umbrellaDir/$dir (has no .git directory)"
-            continue
-        fi
-
-        # Show git status
-        cd "$umbrellaDir/$dir"
-        check="$(git status | grep "nothing to commit, working directory clean")"
-        if [ "$check" ]
-        then
-            echo "$dir - nothing to commit, working directory clean"
-        else
-            echo "$dir - git status:"
-            git status
-            sleep 1
-        fi
-
-    done
-
-done
-
-
-git_exit 0
+if [ "$1" = "push" ]
+then
+    git_push
+    git_exit 0
+fi
 
 
